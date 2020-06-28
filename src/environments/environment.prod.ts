31fc9b741bb44a2c1772b324895a83aacae7e3e6
Vladislav <vldtpk@gmail.com>
2020-06-28 14:24:46 +0300

    deploy on heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: '4bf9ee650055914995cc65f7504f4a4b',
+  apiUrl: 'https://api.openweathermap.org/data/2.5/',
+  imgUrl: 'http://openweathermap.org/img/wn/',
+  reverseGeocodeApi: 'https://api.bigdatacloud.net/data/reverse-geocode-client?',
 };
